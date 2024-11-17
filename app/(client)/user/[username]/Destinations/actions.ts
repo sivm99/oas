9ee681c3f419ca96228a3de87e39979198fd8631
sivm99 <git.v7gr4@simplelogin.fr>
2024-11-17 17:36:39 +0530

    Verify Function Updated

--- app/(client)/user/[username]/Destinations/actions.ts
+++ app/(client)/user/[username]/Destinations/actions.ts
@@ -73,7 +73,7 @@ async function verifyDestination({
       };
     }
     const d = await createRequest(
-      "POST",
+      "GET",
       "/mail/destinations/:destinationID/verify",
       { destinationID },
       token,

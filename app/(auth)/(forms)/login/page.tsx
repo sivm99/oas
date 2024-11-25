4a309147c5cd342320a8db9d3544b2c210f71de4
sivm99 <git.v7gr4@simplelogin.fr>
2024-11-25 21:47:23 +0530

    Login is now static Route

--- app/(auth)/(forms)/login/page.tsx
+++ app/(auth)/(forms)/login/page.tsx
@@ -1,14 +1,15 @@
 import { Separator } from "@/components/ui/separator";
 import AnimatedHero from "@/components/AnimatedHero";
 import LoginForm from "./LoginForm";
-import { fetchUser } from "@/Helper/getData";
-import { redirect } from "next/navigation";
+// import { fetchUser } from "@/Helper/getData";
+// import { redirect } from "next/navigation";
 
 export default async function LoginPage() {
-  const { user } = await fetchUser();
-  if (user) {
-    redirect(`/user/${user.username}`);
-  }
+  // const { user } = await fetchUser();
+  // if (user) {
+  //   redirect(`/user/${user.username}`);
+  // }
+  // this was making it dynamic thats why cancelled it
   return (
     <main className="form_wrapper">
       <div className="opacity-100">
